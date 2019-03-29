0c0128e8e1122933082e368983b0fc164e6968ed
lachlan (zadrok) <lachlan.e.haggart@gmail.com>
2019-03-29 11:47:55 +1100

    all the leds

--- trafficLights.py
+++ trafficLights.py
@@ -1,10 +1,17 @@
 from gpiozero import LED
 from time import sleep
 
-led = LED(3)
+led1 = LED(2)
+led2 = LED(3)
+led3 = LED(4)
 
 while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+  led3.off()
+  led1.on()
+  sleep(1)
+  led1.off()
+  led2.on()
+  sleep(1)
+  led2.off()
+  led3.on()
+  sleep(1)
