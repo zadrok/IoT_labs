f55e69a936c92f44603c6fa7e9b16578be211162
lachlan (zadrok) <lachlan.e.haggart@gmail.com>
2019-04-05 11:37:09 +1100

    add - seiral communication with the Arduino

--- trafficLights.py
+++ trafficLights.py
@@ -1,12 +1,25 @@
-from gpiozero import LED
+from gpiozero import LED, Button
 from time import sleep
+import serial
+port = "/dev/ttyACM0"
 
+# open the serial port to talk over
+s1 = serial.Serial(port,9600)
+# clean the serial port
+s1.flushInput()
+
+# gpio ports led's and button is plugged into
 led1 = LED(2)
 led2 = LED(3)
 led3 = LED(4)
+button = Button(5)
+#  what mode to be showing
+flashMode = False
 
-while True:
+# what to do when the lights are inthe normal mode`
+def normalTrafficLights():
   led3.off()
+  led2.off()
   led1.on()
   sleep(1)
   led1.off()
@@ -15,3 +28,43 @@ while True:
   led2.off()
   led3.on()
   sleep(1)
+
+# what to do when in the flashing mode
+def flashingTrafficLights():
+  led1.off()
+  led2.off()
+  led3.off()
+  sleep(1)
+  led2.on()
+  sleep(1)
+
+# send data to Arduino over the serial
+def signalToArduino(toSend):
+  s1.write(toSend)
+
+# receive data from the Arduino over the serial
+def signalFromArduino():
+  if s1.inWaiting() > 0:
+    return True
+  return False
+
+# loop for order of operations
+while True:
+  # test button
+  if button.is_pressed():
+    # if button is pressed
+    # change mode
+    flashMode = !flashMode
+    # send to Arduino
+    signalToArduino(flashMode)
+
+  # test if signal from arduino
+  if signalFromArduino():
+    # if true change mode
+    flashMode = !flashMode
+
+  # do one loop of the lights sequence
+  if flashMode:
+    flashingTrafficLights()
+  else:
+    normalTrafficLights()
