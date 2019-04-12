145c4bb0bd1d47b8dab785f2ae09844b17e34919
lachlan (zadrok) <lachlan.e.haggart@gmail.com>
2019-04-12 12:06:36 +1000

    fix - some bugs

--- serialTemp.py
+++ serialTemp.py
@@ -8,11 +8,11 @@ arduino.flushInput()
 data = arduino.readline()
 print(data)
 
-dbConn = MySQLdb.connect('localhost','root','tempdb') or die("couldn't connect to the database")
+dbConn = MySQLdb.connect('localhost','root','root','tempdb') or die("couldn't connect to the database")
 print(dbConn)
 
-while dbConn:
-  cursor = dbConn.cusor()
+with dbConn:
+  cursor = dbConn.cursor()
   cursor.execute("INSERT INTO tempLog (Temprature) VALUES (%s)" % (data))
   dbConn.commit()
   cursor.close()
