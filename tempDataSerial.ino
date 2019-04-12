d1814cebe356cb0f435ac2d6c71fd02f4ac531fc
lachlan (zadrok) <lachlan.e.haggart@gmail.com>
2019-04-12 11:01:39 +1000

    add - arduino file to grab temp data and send over serial

---
+++ tempDataSerial.ino
@@ -0,0 +1,13 @@
+int tempPin = A0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = analogRead(tempPin);
+  Serial.write( val );
+  delay(100);
+}
